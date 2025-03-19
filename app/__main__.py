83bb582a80ace8ed4059c4372e975efa42d5d6ba
Ксения Жиленкова <106351895+pqmnyx@users.noreply.github.com>
2025-03-19 21:46:18 +0300

    Add analysis chart

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,10 @@
 import sys
 from PySide6 import QtWidgets
 from PySide6.QtWidgets import QFileDialog
+import signal_analysis
 from interface.window import Ui_MainWindow
 from PySide6.QtCore import Slot
-from PySide6.QtCharts import QScatterSeries, QChart, QChartView
+from PySide6.QtCharts import QScatterSeries, QChart, QChartView, QSplineSeries
 import parsing
 
 
@@ -19,15 +20,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def open_file(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "WAV Files (*.wav)")
         if file_path:
-            data = parsing.parse_wavfile(file_path)
-            print(data)
             scatter_series = QScatterSeries()
             for i in range(10):
                 scatter_series.append(i, i+1)
             
             chart = QChart()
             chart.addSeries(scatter_series)
+            chart.legend().setVisible(0)
             self.horizontalLayout.addWidget(QChartView(chart))
+
+
+            input_signal = parsing.parse_wavfile(file_path)
+
+            ampl_array = signal_analysis.compute_amplitude_spectrum(input_signal)
+            freq_array = signal_analysis.compute_frequency_spectrum(input_signal)
+            line_series_2 = QSplineSeries()
+            for i in range(30):
+                line_series_2.append(ampl_array[i], freq_array[i])
+            
+            chart_2 = QChart()
+            chart_2.addSeries(line_series_2)
+            chart_2.createDefaultAxes()
+            chart_2.legend().setVisible(0)
+            self.horizontalLayout_2.addWidget(QChartView(chart_2))
+
             
 
 
